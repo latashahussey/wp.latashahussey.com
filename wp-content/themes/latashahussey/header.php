ee05fb9f25cdd0bd02c59bca2a8826ed89a0b84c
sugarcookie <latasha.hussey@gmail.com>
2017-06-09 22:33:33 -0500

    add latashahussey theme

---
+++ wp-content/themes/latashahussey/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <!-- Main stylesheet -->
+    <link href="<?php echo get_bloginfo( 'template_directory' );?>/style.css" rel="stylesheet">
+
+
+    <!---Font Awesome Stylesheet-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Devicons Stylesheet-->
+    <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/master/devicon.min.css">
+    <title>LaTasha Hussey | Front-End Developer</title>
+    <?php wp_head();?>
+</head>
+
+<body>
+    <header>
+        <!--Navabar-->
+        <nav class="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
+            <div class="container">
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+                <a class="navbar-brand brand" href="index.html">
+          latasha hussey</a>
+                <div class="collapse navbar-collapse text-uppercase" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link" href="#portfolio">Portfolio <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link" href="#skills">Skills</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!--header container end-->
+    </header>
